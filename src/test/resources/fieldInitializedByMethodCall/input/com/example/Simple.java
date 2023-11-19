f978a407e975e2285d7bfea5a8ecb2cfd2481948
unknown <ln3@njit.edu>
2023-11-19 14:24:11 -0500

    update test file

--- src/test/resources/fieldInitializedByMethodCall/input/com/example/Simple.java
+++ src/test/resources/fieldInitializedByMethodCall/input/com/example/Simple.java
@@ -1,15 +1,10 @@
 package com.example;
 
-import com.example.MyClass;
-import unreal.pack.AClass;
+import com.mypackage.C;
 
 class Simple {
-    // Target method.
-    void bar() {
-        int x  = 5;
-        String y = "hello";
-        AClass z = new AClass();
-        MyClass.process(x);
-        org.testing.ThisClass.process(y, z);
+    final int b = C.get();
+    int bar() {
+        return b++;
     }
 }
