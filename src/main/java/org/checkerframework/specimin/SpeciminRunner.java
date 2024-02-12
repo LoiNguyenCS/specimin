d709d8e9b002e85a0c176a3c1537c3ff377e74e0
unknown <ln3@njit.edu>
2024-02-11 22:22:33 -0500

    update method to add superclass

--- src/main/java/org/checkerframework/specimin/SpeciminRunner.java
+++ src/main/java/org/checkerframework/specimin/SpeciminRunner.java
@@ -213,9 +213,20 @@ public class SpeciminRunner {
       cu.accept(inheritancePreserve, null);
     }
 
+    for (String targetFile : inheritancePreserve.getAddedClass()) {
+      String directoryOfFile = targetFile.replace(".", "/") + ".java";
+      File thisFile = new File(root + directoryOfFile);
+      // classes from JDK are automatically on the classpath, so UnsolvedSymbolVisitor will not
+      // create synthetic files for them
+      if (thisFile.exists()) {
+        parsedTargetFiles.put(directoryOfFile, parseJavaFile(root, directoryOfFile));
+      }
+    }
+
+    Set<String> updatedUsedClass = finder.getUsedClass();
+    updatedUsedClass.addAll(inheritancePreserve.getAddedClass());
     PrunerVisitor methodPruner =
-        new PrunerVisitor(
-            finder.getTargetMethods(), finder.getUsedMembers(), inheritancePreserve.getUsedClass());
+        new PrunerVisitor(finder.getTargetMethods(), finder.getUsedMembers(), updatedUsedClass);
 
     for (CompilationUnit cu : parsedTargetFiles.values()) {
       cu.accept(methodPruner, null);
